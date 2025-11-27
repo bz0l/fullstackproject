ef5d151cfb575baf6008b4b115bbc866c1b69ce3
bz0l <B.Zolana@Wlv.ac.uk>
2025-11-27 15:43:35 +0000

    Update navigation.php

--- php/navigation.php
+++ php/navigation.php
@@ -1,6 +1,13 @@
+<ul id="navigation">
+  <li><a href="#">Page 1</a></li>
+  <li><a href="#">Page 2</a></li>
+  <li><a href="#">Page 3</a></li>
+</ul>
+
 <?php
 // Point to library
-require_once('../vendor/autoload.php');
+require_once '../../vendor/autoload.php';
+
 // Set up Environment
 $loader = new \Twig\Loader\FilesystemLoader('.');
 $twig = new \Twig\Environment($loader);
